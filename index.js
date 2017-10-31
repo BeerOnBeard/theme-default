931aad2d7ad7ac922d02ba3fa9c52fe3cd1ae49b
BeerOnBeard <BeerOnBeard@users.noreply.github.com>
2017-10-31 15:58:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 module.exports = {
     hooks: {
         config: function(config) {
-            config.styles = config.styles || config.pluginsConfig['theme-default'].styles;
+            config.styles = config.styles || config.pluginsConfig['theme-testing-custom'].styles;
 
             return config;
         }
